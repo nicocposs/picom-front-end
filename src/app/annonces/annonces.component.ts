aee8b7a858c6836cd477fb1038d32aa3be61aaa4
nicocposs <msnjus@hotmail.fr>
2022-08-30 19:22:36 +0200

    Css connexion fait

--- src/app/annonces/annonces.component.ts
+++ src/app/annonces/annonces.component.ts
@@ -31,22 +31,27 @@ export class AnnoncesComponent implements OnInit {
       for(let a of annonces){
         let today = new Date();
         if(new Date(a.dateHeureDebut).getTime() > today.getTime()){
+
           let annPassee = new Annonce(a.id,a.titre,new Date(a.dateHeureDebut),new Date(a.dateHeureFin));
           let strJour = new Date(a.dateHeureDebut).toLocaleDateString("fr-FR",{weekday:'short', year: 'numeric', month: 'numeric', day: 'numeric' });
           let newStr = strJour[0].toUpperCase() + strJour.slice(1,strJour.length);
           annPassee.dateDebutSimple = newStr;
+
           let strJour2 = new Date(a.dateHeureFin).toLocaleDateString("fr-FR",{weekday:'short', year: 'numeric', month: 'numeric', day: 'numeric' });
           let newStr2 = strJour2[0].toUpperCase() + strJour2.slice(1,strJour2.length);
           annPassee.dateFinSimple = newStr2;
           this.annoncesFutures.push(annPassee);
         }else if(new Date(a.dateHeureFin).getTime() < today.getTime()){
+
           let annFuture = new Annonce(a.id,a.titre,new Date(a.dateHeureDebut),new Date(a.dateHeureFin));
           let strJour = new Date(a.dateHeureDebut).toLocaleDateString("fr-FR",{weekday:'short', year: 'numeric', month: 'numeric', day: 'numeric' });
           let newStr = strJour[0].toUpperCase() + strJour.slice(1,strJour.length);
           annFuture.dateDebutSimple = newStr;
+
           let strJour2 = new Date(a.dateHeureFin).toLocaleDateString("fr-FR",{weekday:'short', year: 'numeric', month: 'numeric', day: 'numeric' });
           let newStr2 = strJour2[0].toUpperCase() + strJour2.slice(1,strJour2.length);
           annFuture.dateFinSimple = newStr2;
+
           this.annoncesPassees.push(annFuture);
         }
       }
